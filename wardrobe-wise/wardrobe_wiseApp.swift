4e76bc33f29dbea75c033537565c50ee1300fb29
gushee1 <willgushee@gmail.com>
2023-08-22 12:29:03 -0400

    Initial Commit

---
+++ wardrobe-wise/wardrobe_wiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  wardrobe_wiseApp.swift
+//  wardrobe-wise
+//
+//  Created by William Gushee on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct wardrobe_wiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
