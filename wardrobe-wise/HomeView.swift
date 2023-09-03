251401d980967aee6c650a9f15db855ea05bc285
gushee1 <willgushee@gmail.com>
2023-09-03 15:47:02 -0400

    added new files for home page and changed analytics to wardrobe

---
+++ wardrobe-wise/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  wardrobe-wise
+//
+//  Created by William Gushee on 9/3/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("this will be the Home page")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ wardrobe-wise/WardrobeView.swift
@@ -0,0 +1,24 @@
+//
+//  WardrobeView.swift
+//  wardrobe-wise
+//
+//  Created by William Gushee on 9/3/23.
+//
+
+import SwiftUI
+
+struct WardrobeView: View {
+    var body: some View {
+        VStack {
+            Text("this will be the analytics page")
+            Text("This page will contain insights such as price/wear, environmental impact of the purchase(?), and what the item is currrently selling for on other marketplaces")
+        }
+        .padding()
+    }
+}
+
+struct WardrobeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WardrobeView()
+    }
+}
